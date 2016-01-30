be976789e41108955d203c3b01d2b22d7752a085
SurrealJP <jamesrpower@live.com>
2016-01-29 21:07:19 -0400

    Added GameSystem class. Unfinished

---
+++ LoweredExpectations/Assets/Scripts/GameSystem/GameSystem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class GameSystem : MonoBehaviour
+{
+    private eGameState currentGameState = eGameState.Start;
+
+    public delegate void GameStateChangedEvent(eGameState toState);
+    public GameStateChangedEvent OnGameStateChanged;
+
+    private void Awake()
+    {
+        currentGameState = eGameState.Start;
+    }
+	// Use this for initialization
+	void Start () {
+	    
+	}
+
+    public void ChangeState(eGameState toState)
+    {
+        if(toState != currentGameState)
+        {
+            currentGameState = toState;
+            if (OnGameStateChanged != null)
+            {
+                OnGameStateChanged(toState);
+            }
+        }
+    }
+
+    // Run Routine of our game. Let's keep this simple. 
+    // The update loop will do different things based on what game state we're in. 
+    // It will fire a simple event that all of our systems can listen to. 
+	void Update ()
+    {
+        switch (currentGameState)
+        {
+            case eGameState.Start:
+                break;
+            case eGameState.CreateProfile:
+                break;
+            case eGameState.GamePlay:
+                break;
+            case eGameState.Results:
+                break;
+        }
+
+	}
+}
+
+public enum eGameState
+{
+    Start = 0,
+    CreateProfile,
+    GamePlay,
+    Results
+}
