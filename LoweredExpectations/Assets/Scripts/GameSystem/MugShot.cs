305647fc2262698772ec75ca6142bf4334999239
SurrealJP <jamesrpower@live.com>
2016-01-29 22:39:54 -0400

    new file:   LoweredExpectations/Assets/Scripts/GameSystem/MugShot.cs 	new file:   LoweredExpectations/Assets/Scripts/GameSystem/MugShot.cs.meta

---
+++ LoweredExpectations/Assets/Scripts/GameSystem/MugShot.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A character Mugshot
+public class MugShot : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer[] mugShotSprites = new SpriteRenderer[(int)eMugShotComponents.Count];
+
+    // Set the mug shot sprites
+    public void InitMugShot(Sprite[] spriteArray)
+    {
+        for(int i = 0; i < mugShotSprites.Length && i < spriteArray.Length; i++)
+        {
+            mugShotSprites[i].sprite = spriteArray[i];
+        }
+    }
+}
+
+public enum eMugShotComponents
+{
+    Head = 0,
+    Hair,
+    Mouth,
+    Eyes,
+    Ears,
+    Nose,
+    Count
+}
